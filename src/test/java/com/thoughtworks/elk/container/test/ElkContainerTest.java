98f94e659421c3b80133a088491f95bac3bbf26d
Junv <wahyd4@gmail.com>
2013-04-19 17:09:57 +0800

    refactor test case

--- src/test/java/com/thoughtworks/elk/container/test/ElkContainerTest.java
+++ src/test/java/com/thoughtworks/elk/container/test/ElkContainerTest.java
@@ -3,10 +3,13 @@ package com.thoughtworks.elk.container.test;
 import com.thoughtworks.elk.container.ElkContainer;
 import com.thoughtworks.elk.container.exception.ElkContainerException;
 import com.thoughtworks.elk.movie.Director;
+import com.thoughtworks.elk.movie.Movie;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 public class ElkContainerTest {
@@ -21,12 +24,12 @@ public class ElkContainerTest {
     @Test
     public void should_get_a_bean_given_xml_file_without_dependencies() throws ElkContainerException {
         Object movie = elkContainer.getBean("movie");
-        assertThat(movie, notNullValue());
+        assertThat(movie, is(instanceOf(Movie.class)));
     }
 
     @Test
     public void should_get_a_bean_given_xml_file_with_dependencies() throws ElkContainerException {
         Director director = (Director) elkContainer.getBean("director");
-        assertThat(director, notNullValue());
+        assertThat(director, is(instanceOf(Director.class)));
     }
 }
